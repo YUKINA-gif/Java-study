4b1f9765e40e895703cd397243f7e70f03598019
Yukina Nakanishi <yukina-nakanishi@outlook.jp>
2021-08-21 22:22:44 +0900

    first

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO inquiry(name, email, contents, created)
+VALUES('Ethan', 'ethan@example.com', 'Hello', '2019-11-12 08:34:19');
+INSERT INTO inquiry(name, email, contents, created)
+VALUES('Mia', 'mia@example.com', 'GoodMorning', '2019-12-18 12:10:52');
+INSERT INTO inquiry(name, email, contents, created)
+VALUES('Rose', 'rose@example.com', 'GoodEvening', '2019-12-18 15:10:52');
