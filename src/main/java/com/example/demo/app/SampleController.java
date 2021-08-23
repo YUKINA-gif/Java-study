033c0984b5c29136fc59f5344803f4d8c1805b12
Yukina Nakanishi <yukina-nakanishi@outlook.jp>
2021-08-23 12:05:15 +0900

    お問い合わせフォーム作成

--- src/main/java/com/example/demo/app/SampleController.java
+++ src/main/java/com/example/demo/app/SampleController.java
@@ -11,24 +11,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/sample")
-public class SampleController{
-	
+public class SampleController {
+
 	private final JdbcTemplate jdbcTemplate;
-	
+
 	@Autowired
 	public SampleController(JdbcTemplate jdbcTemplate) {
-			this.jdbcTemplate = jdbcTemplate;
+		this.jdbcTemplate = jdbcTemplate;
 	}
 
 	@GetMapping("/test")
 	public String test(Model model) {
-		
+
 		String sql = "SELECT id,name,email FROM inquiry WHERE id = 1";
-		Map<String,Object>map = jdbcTemplate.queryForMap(sql);
-		
-		model.addAttribute("title","Inquiry Form");
-		model.addAttribute("name",map.get("name"));
-		model.addAttribute("email",map.get("email"));
+		Map<String, Object> map = jdbcTemplate.queryForMap(sql);
+
+		model.addAttribute("title", "Inquiry Form");
+		model.addAttribute("name", map.get("name"));
+		model.addAttribute("email", map.get("email"));
 		return "test";
 	}
 }
